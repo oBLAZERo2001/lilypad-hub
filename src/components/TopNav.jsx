df14aca5ad451cc7f2deb8b0cc9d844f1ff3b126
oBLAZERo2001 <hari9112001@gmail.com>
2023-09-19 00:55:22 +0530

    top nav bar intro

---
+++ src/components/TopNav.jsx
@@ -0,0 +1,121 @@
+import { Box, Divider, IconButton, InputBase, Paper } from "@mui/material";
+
+import { AiOutlineSearch } from "react-icons/ai";
+
+export default function TopNav() {
+	return (
+		<Box>
+			<Box
+				sx={{
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "space-between",
+
+					py: "20px",
+				}}
+			>
+				{/* left position */}
+				<Box
+					sx={{
+						display: "flex",
+						alignItems: "center",
+					}}
+				>
+					<Box
+						sx={{
+							width: "30px",
+						}}
+					>
+						<img src="/images/hub.png" width="100%" height="100%" />
+					</Box>
+					<Name />
+					<SearchInputBase />
+				</Box>
+				{/* right position */}
+
+				<Box
+					sx={{
+						display: "flex",
+						alignItems: "center",
+					}}
+				>
+					<Box sx={RightNavs}>Explore</Box>
+				</Box>
+			</Box>
+		</Box>
+	);
+}
+
+const Name = () => {
+	return (
+		<Box
+			sx={{
+				fontSize: "20px",
+				fontWeight: "bold",
+				fontFamily: `'Raleway', sans-serif`,
+				ml: 1,
+			}}
+		>
+			<span
+				style={{
+					color: "#ED72AC",
+				}}
+			>
+				Lily
+			</span>
+			<span
+				style={{
+					color: "#ebebeb",
+				}}
+			>
+				pad
+			</span>
+			<span
+				style={{
+					color: "#2FA7DE",
+				}}
+			>
+				hub
+			</span>
+		</Box>
+	);
+};
+
+const SearchInputBase = () => {
+	return (
+		<Paper
+			component="form"
+			sx={{
+				p: "2px 4px",
+				display: "flex",
+				alignItems: "center",
+				width: 400,
+				ml: 2,
+			}}
+		>
+			<IconButton type="button" sx={{ p: "6px" }} aria-label="search">
+				<AiOutlineSearch />
+			</IconButton>
+			{/* <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" /> */}
+			<InputBase
+				sx={{ ml: 1, flex: 1 }}
+				placeholder="Search Lilypad Hub"
+				inputProps={{ "aria-label": "search lilypad hub" }}
+				size="small"
+			/>
+		</Paper>
+	);
+};
+
+const RightNavs = {
+	color: "#fff",
+	fontSize: "16px",
+	fontWeight: "500",
+	lineHeight: 1.4,
+	mr: 1,
+
+	"&:hover": {
+		cursor: "pointer",
+		color: "rgba(255,255,255,0.8)",
+	},
+};
--- src/screens/Welcome.jsx
+++ src/screens/Welcome.jsx
@@ -1,15 +1,23 @@
 import { Box, FormControl, TextField } from "@mui/material";
 import React from "react";
+import TopNav from "../components/TopNav";
 
 export default function Welcome() {
 	return (
-		<Box>
+		<Box
+			sx={{
+				display: "flex",
+				justifyContent: "center",
+				backgroundImage: `linear-gradient(to right top, #01266b, #012563, #02235c, #052254, #07204c, #02264f, #002b51, #003052, #003d5b, #004a5f, #00565f, #04625c)`,
+				backgroundImage: `linear-gradient(to right top, #d0d0d1, #b1b2c3, #9294b4, #7378a6, #525d97, #4b559d, #454ca1, #4143a5, #6545bb, #8d41cd, #b736db, #e316e3)`,
+			}}
+		>
 			<Box
 				sx={{
-					backgroundImage: `linear-gradient(to right top, #01266b, #012563, #02235c, #052254, #07204c, #02264f, #002b51, #003052, #003d5b, #004a5f, #00565f, #04625c)`,
-					backgroundImage: `linear-gradient(to right top, #d0d0d1, #b1b2c3, #9294b4, #7378a6, #525d97, #4b559d, #454ca1, #4143a5, #6545bb, #8d41cd, #b736db, #e316e3)`,
 					width: "100vw",
-					height: "100vh",
+					maxWidth: "1260px",
+
+					minHeight: "100vh",
 
 					backgroundSize: "cover",
 					backgroundPosition: "top",
@@ -19,15 +27,16 @@ export default function Welcome() {
 					WebkitFontSmoothing: "antialiased",
 				}}
 			>
-				{/*  left landing text */}
+				<TopNav />
 				<Box
 					sx={{
 						pt: "10vh",
-						px: "10vw",
 						display: "flex",
 						justifyContent: "space-between",
 					}}
 				>
+					{/*  left landing text */}
+
 					<Box sx={LandingBox}>
 						<Box
 							sx={{
